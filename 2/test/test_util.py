224920172bc597ed714cebfd6a8c5b8bf56ed89a
Yudai Udagawa <nimius.rd@gmail.com>
2017-06-24 18:37:22 +0900

    2

---
+++ 2/test/test_util.py
@@ -0,0 +1,20 @@
+import unittest
+from util import util
+
+
+class UtilTest(unittest.TestCase):
+    def test_is_url(self):
+        cases = [
+            'http://example.com',
+            'https://example.com',
+            'https://e-ample.com',
+            'https://e_ample.com',
+            'https://ex/ample.com',
+            'https://e%3ample.com',
+            'http://example.com?q=hoge&p=fuga',
+            'http://example.com#piyo'
+        ]
+        for case in cases:
+            with self.subTest(string=case):
+                print(case)
+                self.assertTrue(util.is_url(case))
