055e16486ca565de3bc2c36759ebe62a435e084d
Yudai Udagawa <nimius.rd@gmail.com>
2017-06-25 14:21:05 +0900

    match -> fullmatch

--- 4/util/util.py
+++ 4/util/util.py
@@ -3,4 +3,4 @@ from typing import Match
 
 
 def is_url(src: str) -> Match[str]:
-    return re.match(r'^https?://[0-9a-zA-Z/:%$&()~.=+\-_?#]+$', src)
+    return re.fullmatch(r'^https?://[0-9a-zA-Z/:%$&()~.=+\-_?#]+$', src)
